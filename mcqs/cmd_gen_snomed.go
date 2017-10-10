f9ec30b0b5a221b20cf5190ff2b4013ffd7ed169
Mark Wardle <mark@wardle.org>
2017-10-10 14:02:34 +0100

    Finish command to build cached SNOMED data files.

--- mcqs/cmd_gen_snomed.go
+++ mcqs/cmd_gen_snomed.go
@@ -2,11 +2,16 @@ package mcqs
 
 import (
 	"database/sql"
+	"encoding/csv"
 	"fmt"
+	"github.com/briandowns/spinner"
 	"github.com/wardle/go-snomed/snomed"
-	"math/rand"
+	"io"
+	"log"
+	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
 // define simple connection settings statically for this hack. TODO: move to configuration file
@@ -23,7 +28,7 @@ const (
 	child_concept_id=concept_id 
 	and
 	concept_id in (select child_concept_id from t_cached_parent_concepts where parent_concept_id=%d)
-	group by concept_id limit 10`
+	group by concept_id`
 	sctDiagnosisRoot           = 64572001  // root concept of all diagnoses
 	sctClinicalObservationRoot = 250171008 // root concept of all clinical observations
 )
@@ -31,31 +36,50 @@ const (
 // GenerateSnomedCT generates a set of useful intermediate files containing pre-computed and cached
 // SNOMED-CT data, useful for sharing with others without depending on a dedicated terminology server.
 func GenerateSnomedCT() {
-	fmt.Println("Connecting to database..")
+	spinner := spinner.New(spinner.CharSets[9], 100*time.Millisecond) // Build our new spinner
+	spinner.Start()
 	dbinfo := fmt.Sprintf("user=%s dbname=%s sslmode=disable", dbUser, dbName)
 	db, err := sql.Open(dbDriver, dbinfo)
 	checkErr(err)
 	defer db.Close()
 
-	fmt.Println("# Querying")
-	diagnoses := fetchConcepts(db, sctDiagnosisRoot)
-	clinicalFindings := fetchConcepts(db, sctClinicalObservationRoot)
-	nClinicalFindings := len(clinicalFindings)
-	fmt.Printf("Number of diagnoses: %d\n", len(diagnoses))
-	fmt.Printf("Number of clinical observations: %d\n", nClinicalFindings)
+	writeConceptsCsv(db, sctDiagnosisRoot, "sct_diagnoses.csv")
+	spinner.Reverse()
+	writeConceptsCsv(db, sctClinicalObservationRoot, "sct_findings.csv")
+	spinner.Stop()
+}
 
-	fmt.Println("Writing csv...")
-	//w := csv.NewWriter(os.Stdout)
-	// randomly select a few clinical observations for each diagnosis.
-	// this is fake data obviously!
-	for _, diagnosis := range diagnoses {
-		numFeatures := rand.Intn(10)
-		features := make([]*snomed.Concept, numFeatures)
-		for i := 0; i < numFeatures; i++ {
-			features[i] = clinicalFindings[rand.Intn(nClinicalFindings)]
+// perform a query and write results to filename as csv
+func writeConceptsCsv(db *sql.DB, root int, filename string) {
+	concepts := fetchConcepts(db, root)
+	file, err := os.Create(filename)
+	defer file.Close()
+	checkErr(err)
+	writeToCsv(file, concepts)
+
+}
+
+func writeToCsv(w io.Writer, concepts map[int]*snomed.Concept) {
+	w2 := csv.NewWriter(w)
+	for _, concept := range concepts {
+		record := make([]string, 5)
+		record[0] = strconv.Itoa(concept.ConceptID)
+		record[1] = concept.FullySpecifiedName
+		record[2] = concept.Status.Title
+		record[3] = listItoA(concept.Parents)
+		parentFsns := make([]string, 0)
+		for _, parentID := range concept.Parents {
+			parent := concepts[parentID]
+			if parent != nil {
+				parentFsns = append(parentFsns, parent.FullySpecifiedName)
+			}
 		}
-		fmt.Println(diagnosis.FullySpecifiedName)
-		fmt.Println(features)
+		record[4] = strings.Join(parentFsns, ",")
+		csvError := w2.Write(record)
+		if csvError != nil {
+			log.Fatalf("Failed to write CSV file: %s", csvError)
+		}
+		w2.Flush()
 	}
 	//fmt.Printf("Status: %s", snomed.LookupStatus(1))
 
@@ -64,7 +88,6 @@ func GenerateSnomedCT() {
 // fetch all of the concepts within SNOMED-CT beneath the given root
 func fetchConcepts(db *sql.DB, root int) map[int]*snomed.Concept {
 	sql := fmt.Sprintf(sqlSelectConceptFmt, root)
-	fmt.Print(sql)
 	rows, err := db.Query(sql)
 	checkErr(err)
 	concepts := make(map[int]*snomed.Concept)
@@ -95,6 +118,15 @@ func listAtoi(list string) []int {
 		}
 	}
 	return r
+}
+
+func listItoA(list []int) string {
+	r := make([]string, 0)
+	for _, i := range list {
+		s := strconv.Itoa(i)
+		r = append(r, s)
+	}
+	return strings.Join(r, ",")
 }
 
 func checkErr(err error) {
--- mcqs/cmd_gen_snomed_test.go
+++ mcqs/cmd_gen_snomed_test.go
@@ -1,17 +1,27 @@
 package mcqs
 
-import "testing"
-import "reflect"
+import (
+	"reflect"
+	"strings"
+	"testing"
+)
 
 func TestListAtoi(t *testing.T) {
-	testAtoi(t, "123,456,789,123456789", []int{123, 456, 789, 123456789})
-	testAtoi(t, "123,456,789, 123456789", []int{123, 456, 789, 123456789})
-	testAtoi(t, "aaa,vbb,cc,123", []int{123})
+	testAtoi(t, "123,456,789,123456789", []int{123, 456, 789, 123456789}, true)
+	testAtoi(t, "123,456,789, 123456789", []int{123, 456, 789, 123456789}, true)
+	testAtoi(t, "aaa,vbb,cc,123", []int{123}, false)
 }
 
-func testAtoi(t *testing.T, input string, expected []int) {
+// test conversion of a comma-delimited string of integers to a slice, optionally testing roundtripping
+func testAtoi(t *testing.T, input string, expected []int, roundtrip bool) {
 	r := listAtoi(input)
 	if reflect.DeepEqual(r, expected) == false {
 		t.Errorf("Failed to parse: %s. Parsed to %v", input, r)
+	}
+	if roundtrip {
+		v := listItoA(expected)
+		if reflect.DeepEqual(v, strings.Replace(input, " ", "", -1)) == false {
+			t.Errorf("Failed to parse: %v. Parsed to %s", expected, v)
+		}
 	}
 }
