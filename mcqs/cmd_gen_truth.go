fc754ec896a45719d0f80d8fad99dd4f40379f36
Mark Wardle <mark@wardle.org>
2017-10-15 21:34:16 +0100

    Initial concepts for fake truth dataset.

--- mcqs/cmd_gen_truth.go
+++ mcqs/cmd_gen_truth.go
@@ -1,7 +1,20 @@
 package mcqs
 
+import (
+	"bitbucket.org/wardle/go-snomed/snomed"
+)
+
+type FakeTruth struct {
+	Diagnosis *snomed.Concept
+	Parents   []*snomed.Concept // convenience pointers to parents
+	Problems  []*snomed.Concept // problems for this diagnosis
+}
+
 // GenerateFakeTruth takes the SNOMED-CT ontology and uses it to build a fake "truth" model
 // that represents the clinical findings that are seen in each type of diagnosis.
+//
+// While simply generating random problems for each diagnosis might be one approach, it is incorrect as
+// we have a clear subsumption IS-A hierarchy which can be used. As such, related diagnostic concepts
+// should share similar clinical problems in order to generate reasonable fake data.
 func GenerateFakeTruth(dataset SnomedDataset) {
-
 }
