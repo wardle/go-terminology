04e670c481afe3a1313a89cc48066f4a4960c76d
Mark Wardle <mark@wardle.org>
2017-11-18 21:59:40 +0000

    Switch backing cache to a concurrent safe version.

--- snomed/cache.go
+++ snomed/cache.go
@@ -1,40 +1,37 @@
 package snomed
 
 import (
-	"fmt"
+	"sync"
 )
 
-// NaiveCache is an extraordinarily naive in-memory cache used for development.
+// NaiveCache is an fairly naive in-memory cache used for development.
 // It is designed to store arbitrary SNOMED-CT entities such as Concepts, Descriptions and Relationships.
 // To make it easier to use, there are convenience methods to do the type-casting to put and get objects of
 // these types which will simply wrap a better future implementation when implemented.
-// TODO: use a better cache behind-the-scenes.
+// This now is backed by a concurrent map instead ready for experimenting with concurrency.
+// TODO: use an even better cache behind-the-scenes perhaps with persistence to filesystem?
 type NaiveCache struct {
-	cache map[int]interface{}
+	cache sync.Map
 }
 
 // NewCache creates a new cache
 func NewCache() *NaiveCache {
-	return &NaiveCache{make(map[int]interface{})}
+	return &NaiveCache{}
 }
 
 // Clear clears the cache
-func (nc NaiveCache) Clear() {
-	nc.cache = make(map[int]interface{})
+func (nc *NaiveCache) Clear() {
+	nc.cache = *new(sync.Map)
 }
 
 // Get fetches a generic object from the cache using the specified identifier
 // returning the object and a boolean indicating success (or not)
-func (nc NaiveCache) Get(id int) (interface{}, bool) {
-	value := nc.cache[id]
-	if value != nil {
-		return value, true
-	}
-	return nil, false
+func (nc *NaiveCache) Get(id int) (interface{}, bool) {
+	return nc.cache.Load(id)
 }
 
 // GetOrElse fethes a generic object from the cache or performs the callback specified, caching the result
-func (nc NaiveCache) GetOrElse(id int, f func(int) (interface{}, error)) (interface{}, error) {
+func (nc *NaiveCache) GetOrElse(id int, f func(int) (interface{}, error)) (interface{}, error) {
 	value, success := nc.Get(id)
 	if success {
 		return value, nil
@@ -48,17 +45,17 @@ func (nc NaiveCache) GetOrElse(id int, f func(int) (interface{}, error)) (interf
 }
 
 // Put stores a generic object into the cache using the specified identifier
-func (nc NaiveCache) Put(id int, value interface{}) {
-	nc.cache[id] = value
+func (nc *NaiveCache) Put(id int, value interface{}) {
+	nc.cache.Store(id, value)
 }
 
 // PutConcept stores a concept in the cache
-func (nc NaiveCache) PutConcept(conceptID int, concept *Concept) {
+func (nc *NaiveCache) PutConcept(conceptID int, concept *Concept) {
 	nc.Put(conceptID, concept)
 }
 
 // GetConcept fetches a concept from the cache
-func (nc NaiveCache) GetConcept(conceptID int) (*Concept, bool) {
+func (nc *NaiveCache) GetConcept(conceptID int) (*Concept, bool) {
 	value, success := nc.Get(conceptID)
 	if success {
 		concept, ok := value.(*Concept)
@@ -70,7 +67,7 @@ func (nc NaiveCache) GetConcept(conceptID int) (*Concept, bool) {
 }
 
 // GetConceptOrElse fetches a concept from the cache or performs the callback specified, caching the result
-func (nc NaiveCache) GetConceptOrElse(conceptID int, f func(conceptID int) (interface{}, error)) (*Concept, error) {
+func (nc *NaiveCache) GetConceptOrElse(conceptID int, f func(conceptID int) (interface{}, error)) (*Concept, error) {
 	v, err := nc.GetOrElse(conceptID, f)
 	if err != nil {
 		return nil, err
@@ -79,12 +76,12 @@ func (nc NaiveCache) GetConceptOrElse(conceptID int, f func(conceptID int) (inte
 }
 
 // PutDescription stores a description in the cache
-func (nc NaiveCache) PutDescription(descriptionID int, description *Description) {
+func (nc *NaiveCache) PutDescription(descriptionID int, description *Description) {
 	nc.Put(descriptionID, description)
 }
 
 // GetDescription fetches a description from the cache
-func (nc NaiveCache) GetDescription(descriptionID int) (*Description, bool) {
+func (nc *NaiveCache) GetDescription(descriptionID int) (*Description, bool) {
 	value, success := nc.Get(descriptionID)
 	if success {
 		description, ok := value.(*Description)
@@ -96,7 +93,7 @@ func (nc NaiveCache) GetDescription(descriptionID int) (*Description, bool) {
 }
 
 // GetDescriptionOrElse fetches a description from the cache or performs the callback specified, caching the result
-func (nc NaiveCache) GetDescriptionOrElse(descriptionID int, f func(descriptionID int) (interface{}, error)) (*Description, error) {
+func (nc *NaiveCache) GetDescriptionOrElse(descriptionID int, f func(descriptionID int) (interface{}, error)) (*Description, error) {
 	v, err := nc.GetOrElse(descriptionID, f)
 	if err != nil {
 		return nil, err
@@ -105,12 +102,12 @@ func (nc NaiveCache) GetDescriptionOrElse(descriptionID int, f func(descriptionI
 }
 
 // PutRelationship stores a relationship in the cache
-func (nc NaiveCache) PutRelationship(descriptionID int, description *Description) {
+func (nc *NaiveCache) PutRelationship(descriptionID int, description *Description) {
 	nc.Put(descriptionID, description)
 }
 
 // GetRelationship fetches a relationship from the cache
-func (nc NaiveCache) GetRelationship(relationshipID int) (*Relationship, bool) {
+func (nc *NaiveCache) GetRelationship(relationshipID int) (*Relationship, bool) {
 	value, success := nc.Get(relationshipID)
 	if success {
 		relationship, ok := value.(*Relationship)
@@ -122,14 +119,10 @@ func (nc NaiveCache) GetRelationship(relationshipID int) (*Relationship, bool) {
 }
 
 // GetRelationshipOrElse fetches a relationship from the cache or performs the callback specified, caching the result
-func (nc NaiveCache) GetRelationshipOrElse(relationshipID int, f func(relationshipID int) (interface{}, error)) (*Relationship, error) {
+func (nc *NaiveCache) GetRelationshipOrElse(relationshipID int, f func(relationshipID int) (interface{}, error)) (*Relationship, error) {
 	v, err := nc.GetOrElse(relationshipID, f)
 	if err != nil {
 		return nil, err
 	}
 	return v.(*Relationship), nil
-}
-
-func (nc NaiveCache) String() string {
-	return fmt.Sprintf("Cache with %d entries", len(nc.cache))
 }
