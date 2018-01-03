23f6a143f17a044df03c4fc9344675045e127eed
Mark Wardle <mark@wardle.org>
2018-01-03 16:08:56 +0000

    Add benchmarks to testing.
    Preparation for working to optimise and cache common operations when building data structures.

--- db/service_test.go
+++ db/service_test.go
@@ -3,7 +3,6 @@ package db_test
 import (
 	"bitbucket.org/wardle/go-snomed/db"
 	"golang.org/x/text/language"
-	//"bitbucket.org/wardle/go-snomed/rf2"
 	"os"
 	"testing"
 )
@@ -12,19 +11,19 @@ const (
 	dbFilename = "../snomed.db"
 )
 
-func hasLiveDatabase(t *testing.T) {
+func setUp(tb testing.TB) *db.Snomed {
 	if _, err := os.Stat(dbFilename); os.IsNotExist(err) { // skip these tests if no working live snomed db
-		t.Skipf("Skipping tests against a live database. To run, create a database named %s", dbFilename)
+		tb.Skipf("Skipping tests against a live database. To run, create a database named %s", dbFilename)
 	}
+	bolt, err := db.NewBoltService(dbFilename)
+	if err != nil {
+		tb.Fatal(err)
+	}
+	return &db.Snomed{Service: bolt, Language: language.BritishEnglish}
 }
 
 func TestService(t *testing.T) {
-	hasLiveDatabase(t)
-	bolt, err := db.NewBoltService(dbFilename)
-	if err != nil {
-		t.Fatal(err)
-	}
-	snomed := &db.Snomed{Service: bolt, Language: language.BritishEnglish}
+	snomed := setUp(t)
 	ms, err := snomed.GetConcept(24700007)
 	if err != nil {
 		t.Fatal(err)
@@ -44,5 +43,40 @@ func TestService(t *testing.T) {
 	}
 	if !snomed.IsA(ms, 6118003) {
 		t.Fatal("Multiple sclerosis not correctly identified as a type of demyelinating disease")
+	}
+}
+
+func BenchmarkGetConceptAndDescriptions(b *testing.B) {
+	snomed := setUp(b)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		ms, err := snomed.GetConcept(24700007)
+		if err != nil {
+			b.Fatal(err)
+		}
+		_, err = snomed.GetDescriptions(ms)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkIsA(b *testing.B) {
+	snomed := setUp(b)
+	ms, err := snomed.GetConcept(24700007)
+	if err != nil {
+		b.Fatal(err)
+	}
+	b.ResetTimer()
+	var (
+		isDemyelinating  bool
+		isPharmaceutical bool
+	)
+	for i := 0; i < b.N; i++ {
+		isDemyelinating = snomed.IsA(ms, 6118003)
+		isPharmaceutical = snomed.IsA(ms, 373873005)
+	}
+	if isDemyelinating == false || isPharmaceutical == true {
+		b.Fatal("MS misclassified using IS-A hierarchy")
 	}
 }
