21f00087ca68b1b86842fcd0511962b115b95995
Mark Wardle <mark@wardle.org>
2018-01-09 16:51:09 +0000

    We’ll need to reference this language set.
    This will be used to map language codes into their installed reference sets and so derive preferred terms.

--- snomed/semantic.go
+++ snomed/semantic.go
@@ -4,4 +4,7 @@ package snomed
 const (
 	// IS-A is the commonest type of relationship
 	IsAConceptID Identifier = 116680003
+
+	// The British Engligh Language Reference Set
+	BritishEnglishLanguageReferenceSetConceptID Identifier = 900000000000508004
 )
