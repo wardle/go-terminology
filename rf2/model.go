234165950489caaa6fcc73a53e4430db6587fec4
Mark Wardle <mark@wardle.org>
2018-01-09 20:34:40 +0000

    Add copyright notices.

--- rf2/model.go
+++ rf2/model.go
@@ -15,6 +15,21 @@
 // Delta	The files representing each type of component contain only component versions created since the previous release. Each component version in a delta release represents either a new component or a change to an existing component.
 //
 // NB: The associated import functionality imports only from snapshot files.
+//
+// // Copyright 2018 Mark Wardle / Eldrix Ltd
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//
 package rf2
 
 import (
